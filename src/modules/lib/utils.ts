2bf0fcc08416423dfbcf66c9183f964cb98fe51c
josecapewa <capewajose@gmail.com>
2025-02-06 15:17:45 +0100

    feat: :sparkles: update user model with new fields and validations

--- src/modules/lib/utils.ts
+++ src/modules/lib/utils.ts
@@ -1,6 +1,3 @@
-import { userModel } from "../models/user";
-import { cpanelService } from "../services/cpanel";
-
 export function validateBiFormat(biNumber: string) {
   const biRegex = /^\d{9}[A-Za-z]{2}\d{3}$/;
   return biRegex.test(biNumber);
@@ -13,24 +10,6 @@ export function getFirstAndLastName(name: string) {
   return `${firstName} ${lastName}`;
 }
 
-export async function generateUsername(name: string): Promise<string> {
-  let baseUsername = getFirstAndLastName(name)
-    .toLowerCase()
-    .normalize("NFD")
-    .replace(/[\u0300-\u036f]/g, "")
-    .replace(/[^a-z0-9]/g, "");
-
-  let username = baseUsername;
-  let counter = 1;
-
-  while (await userModel.userExists(username)) {
-    username = `${baseUsername}${counter}`;
-    counter++;
-  }
-
-  return username;
-}
-
 export function formatCamelCaseToTitle(text: string): string {
   if (!text) return "";
 
@@ -39,39 +18,8 @@ export function formatCamelCaseToTitle(text: string): string {
   return spacedText.trim().replace(/^./, (str) => str.toUpperCase());
 }
 
-export async function generateUniqueEmail(email: string): Promise<string> {
-  let [baseUsername, domain] = email.split("@");
-  let username = baseUsername;
-  let counter = 1;
-
-  while (await cpanelService.doesEmailExist(`${username}@${domain}`)) {
-    username = `${baseUsername}${counter}`;
-    counter++;
-  }
-
-  return `${username}@${domain}`;
-}
-
 export const isPasswordStrongEnought = (pass: string) => {
-  return pass.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&*!])[A-Za-z\d@#$%^&*!]{8,}$/)
-}
-
-//Special Exams functions
-
-export const formatSubjectName = (nome: string) => {
-  const semEspaco = nome.trimEnd().trimStart();
-  const palavras = semEspaco.split(" ");
-
-  const formatadas = palavras.filter((p) => p.length > 2);
-
-  if (formatadas.length > 2) {
-    return formatadas
-      .map((p, ind) => {
-        return `${p[0].toUpperCase()}${
-          ind === formatadas.length - 1 ? "" : "."
-        }`;
-      })
-      .join(" ");
-  }
-  return nome;
+  return pass.match(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&*!])[A-Za-z\d@#$%^&*!]{8,}$/
+  );
 };
--- src/modules/models/user.ts
+++ src/modules/models/user.ts
@@ -2,42 +2,37 @@ import { Usuario, Prisma } from "@prisma/client";
 import prisma from "../lib/prisma";
 import { BaseModel, TBaseGetAllParams } from "./base";
 
-type UserDataToUpdateWithPermissions = {
-  permissoes_telas?: {
-    permissao: "VISUALISAR" | "CRIAR" | "EDITAR" | "ELIMINAR";
-    caminho_tela: string;
-  }[];
-  is_master?: boolean;
-  nome?: string;
-  nome_usuario?: string;
-  genero?: boolean;
-};
-
 const userIncludes = {
-  pessoa: {
+  cartao_usuario: true,
+  trocas: {
     include: {
-      telefone: true,
-      email: true,
-      aluno: {
-        include: {
-          encarregado: true,
-        },
-      },
+      referencia: true,
     },
   },
-  permissoes_telas: true,
 } as Prisma.UsuarioInclude;
 
 class UserModel extends BaseModel<Usuario> {
   model = prisma.usuario;
   include = userIncludes;
 
+  create = async (data: Omit<Usuario, "id" | "tipo" | "foto" | "pontos">) => {
+    return await this.model.create({ data, include: this.include });
+  };
+
   getAll = async ({ skip, take, filter }: TBaseGetAllParams) => {
     this.getAllWherClause = {
-      nome_usuario: { contains: filter },
-      pessoa: {
-        nome: { contains: filter },
-      },
+      OR: [
+        {
+          nome: {
+            contains: filter,
+          },
+        },
+        {
+          email: {
+            contains: filter,
+          },
+        },
+      ],
     } as Prisma.UsuarioWhereInput;
 
     return await this.model.findMany({
@@ -48,96 +43,10 @@ class UserModel extends BaseModel<Usuario> {
     });
   };
 
-  create = async (data: Omit<Usuario, "id">) => {
-    return await this.model.create({ data, include: this.include });
-  };
-  simpleUpdate = async (
-    id: string,
-    {
-      nome_usuario,
-      email,
-      foto,
-      password,
-      telefone,
-    }: Partial<Usuario & { telefone?: string; email?: string; foto?: string }>
-  ) => {
-    return await this.model.update({
-      where: { id },
-      data: {
-        nome_usuario: nome_usuario,
-        password: password,
-        foto: foto,
-        pessoa: {
-          update: {
-            telefone: {
-              update: {
-                numero: telefone,
-              },
-            },
-            email: email
-              ? {
-                  upsert: {
-                    create: {
-                      email: email,
-                    },
-                    update: {
-                      email: email,
-                    },
-                  },
-                }
-              : undefined,
-          },
-        },
-      },
-      include: this.include,
-    });
-  };
-
-  getData = async (identification: string) => {
+  getByEmail = async (email: string) => {
     return await prisma.usuario.findFirst({
-      where: {
-        OR: [
-          { nome_usuario: identification },
-          { pessoa: { identificacao: { numero: identification } } },
-          { pessoa: { email: { email: identification } } },
-        ],
-      },
+      where: { email },
       include: this.include,
-    });
-  };
-
-  userExists = async (field: string): Promise<boolean> => {
-    const user = await prisma.usuario.findFirst({
-      where: {
-        OR: [{ nome_usuario: field }, { id_pessoa: field }],
-      },
-      include: this.include,
-    });
-    return !!user;
-  };
-  updateWithPermissions = async (
-    id: string,
-    data: UserDataToUpdateWithPermissions
-  ) => {
-    return await prisma.usuario.update({
-      where: { id },
-      data: {
-        nome_usuario: data.nome_usuario,
-        is_master: data.is_master,
-        pessoa: {
-          update: {
-            nome: data.nome,
-          },
-        },
-        permissoes_telas: data.permissoes_telas
-          ? {
-              deleteMany: {},
-              createMany: {
-                data: data.permissoes_telas,
-              },
-            }
-          : undefined,
-      },
     });
   };
 }
